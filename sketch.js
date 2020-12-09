0cdade6e463d622a58f4a91305323603af8e3db6
puppets101 <oscand92@gmail.com>
2020-12-09 08:54:57 +0100

    Added background + img

--- sketch.js
+++ sketch.js
@@ -1,3 +1,14 @@
-function setup() {}
+let img;
+function preload() {
+  img = loadImage("assets/christmas_tree.png");
+}
 
-function draw() {}
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  background("brown");
+  imageMode(CENTER);
+  image(img, innerWidth / 2, innerHeight / 2, 400, 400);
+}
